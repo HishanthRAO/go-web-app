8de4bcda9a371c32dda4fcc80272b073d4ff543b
HishanthRAO <hishanth66@gmail.com>
2026-03-19 00:41:32 +0100

    Add Go web app with Kubernetes manifests

--- main.go
+++ main.go
@@ -1,39 +1,14 @@
 package main
 
 import (
-	"log"
-	"net/http"
+    "fmt"
+    "net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	// Render the home html page from static folder
-	http.ServeFile(w, r, "static/home.html")
-}
-
-func coursePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page
-	http.ServeFile(w, r, "static/courses.html")
-}
-
-func aboutPage(w http.ResponseWriter, r *http.Request) {
-	// Render the about html page
-	http.ServeFile(w, r, "static/about.html")
-}
-
-func contactPage(w http.ResponseWriter, r *http.Request) {
-	// Render the contact html page
-	http.ServeFile(w, r, "static/contact.html")
-}
-
 func main() {
-
-	http.HandleFunc("/home", homePage)
-	http.HandleFunc("/courses", coursePage)
-	http.HandleFunc("/about", aboutPage)
-	http.HandleFunc("/contact", contactPage)
-
-	err := http.ListenAndServe("0.0.0.0:8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "<h1>Hello from DevOpsified Go Web App!</h1><p>Built by Hishanth Rao</p>")
+    })
+    fmt.Println("Server running on port 8080")
+    http.ListenAndServe(":8080", nil)
 }
